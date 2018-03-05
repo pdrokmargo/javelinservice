f269a27b5702868971b19d245253ddb7de348176
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-03-04 22:53:24 -0500

    asd

--- app/Models/DeliveryContract.php
+++ app/Models/DeliveryContract.php
@@ -9,11 +9,18 @@ class DeliveryContract extends Model
     public $timestamps = false;
     protected $table = 'delivery_contracts';
     protected $fillable = [
+        'name',
+        'population_type_id',
+        'description',
         'contract_type_id',
         'value',
         'validity',
         'level_care_id',
-        'state'
+        'state',
+        'pharmadrug_monopoly',
+        'pharmadrug_control',
+        'cooled_products',
+        'event'
     ];
     protected $hidden = [];
 
