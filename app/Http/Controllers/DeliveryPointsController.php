97fbc270ca7361fcab8f5431e6c31d00f3f09362
Pedro Camargo <pdro_kmargo@hotmail.com>
2018-03-08 00:21:10 -0500

    rr

--- app/Http/Controllers/DeliveryPointsController.php
+++ app/Http/Controllers/DeliveryPointsController.php
@@ -48,9 +48,10 @@ class DeliveryPointsController extends Controller
                     $contracts = \App\Models\DeliveryContract::find($dc['delivery_contract_id']);    
                     //return response()->json(['status'=>'success', "message"=>'', "data" => $dc['delivery_contract_id'], "data2" => $contracts->name ], 200);    
                     //$contracts = \App\Models\DeliveryContract::where('id', $dc['delivery_contract_id'])->first();
-                    $dc = json_decode($dc);
-                    $dc["delivery_contract_name"] = $contracts->name;
                     $dc = json_encode($dc);
+                    $dc["delivery_contract_name"] = $contracts->name;
+                    $dc = json_decode($dc, true);
+                    
                     
                 } 
             }else{
