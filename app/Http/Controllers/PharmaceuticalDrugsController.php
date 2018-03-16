fe68e8856f593cbe046c71a5e7f09e9fbf2659fa
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-03-15 23:01:40 -0500

    asd

--- app/Http/Controllers/PharmaceuticalDrugsController.php
+++ app/Http/Controllers/PharmaceuticalDrugsController.php
@@ -84,7 +84,7 @@ class PharmaceuticalDrugsController extends Controller
     public function show($id)
     {
         $pd = \App\Models\PharmaceuticalDrug::find($id);
-        $ai = ActiveIngredientsPharmaceuticalDrugs::where('pharmaceutical_drug_id',$pd->id);
+        $ai = \App\Models\ActiveIngredientsPharmaceuticalDrugs::where('pharmaceutical_drug_id',$pd->id);
         $data = [
             "model" => $pd,
             "active_ingredients" => $ai
