0089a966914946397cce698f30e5c1df79b62565
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-03-15 23:29:47 -0500

    asd

--- app/Http/Controllers/PharmaceuticalDrugsController.php
+++ app/Http/Controllers/PharmaceuticalDrugsController.php
@@ -84,8 +84,7 @@ class PharmaceuticalDrugsController extends Controller
     public function show($id)
     {
         $pd = \App\Models\PharmaceuticalDrug::find($id);
-        $ai = \App\Models\ActiveIngredientsPharmaceuticalDrugs::where('pharmaceutical_drug_id','=',$id);
-        $ai->append(['measurement_unit','pharmaceutical_drug']);
+        $ai = \App\Models\ActiveIngredientsPharmaceuticalDrugs::where('pharmaceutical_drug_id','=',$id)->append(['measurement_unit','pharmaceutical_drug']);
         $data = [
             "model" => $pd,
             "active_ingredients" => $ai
