cd05a713cd2ed3fd7fdd2c6ea2888db3c355325c
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-03-25 00:13:11 -0500

    asd

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -97,7 +97,8 @@ class UsersController extends Controller
               $join->on('u.id', '=', 'uc.user_id');
             })->leftJoin('companies as c', 'c.id', '=', 'u.company_default_id')
             ->leftJoin('user_profiles as up', 'uc.user_profile_id', '=', 'up.id')
-            ->select(DB::raw('u.id, u.firstname, u.lastname, u.username, u.email, u.password, u.status, u.last_access, u.url_profile_photo, u.company_default_id, up.up_description as user_profile, c.name as company'));
+            ->select(DB::raw('u.id, u.firstname, u.lastname, u.username, u.email, u.password, u.status, u.last_access, u.url_profile_photo, u.company_default_id, up.up_description as user_profile, c.name as company'))
+            ->where('up.up_description',$type);
 
             if ($search!='') {
                 $query=$query->whereRaw("concat(lower(u.firstname), ' ', lower(u.lastname)) like ? or lower(u.username) like ? or 
