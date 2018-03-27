0bce243fa2e01c1c7691bf8fbbe14e86b79b537d
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-03-27 17:55:32 -0500

    asd

--- app/Models/ContractPoint.php
+++ app/Models/ContractPoint.php
@@ -22,11 +22,11 @@ class ContractPoint extends Model
 
     public function delivery_ontracts()
     {
-        return $this->hasMany('App\Models\DeliveryContract','delivery_contracts_id', 'id');
+        return $this->hasMany('App\Models\DeliveryContract', 'id','delivery_contracts_id');
     }
 
     public function delivery_points()
     {
-        return $this->hasMany('App\Models\DeliveryPoint','delivery_points_id', 'id');
+        return $this->hasMany('App\Models\DeliveryPoint', 'id','delivery_points_id');
     }
 }
