4bd9365371df2d2d07aad191bce7f5c25a67155f
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-03-27 14:49:48 -0500

    asd

--- app/Models/DeliveryPoint.php
+++ app/Models/DeliveryPoint.php
@@ -31,18 +31,13 @@ class DeliveryPoint extends Model
         $contracts = [];
         if(!empty($this->delivery_contracts))
         {
-            if(!is_array($this->delivery_contracts)){
+            if(!is_array($this->delivery_contracts || [])){
                 $this->delivery_contracts = [];
             }
             
             foreach ($this->delivery_contracts as $dc)
             {
-                
                 $contract = \App\Models\DeliveryContract::select(['id','name','event','capita','pgp'])->find($dc['id']);
-
-                /*if(!$dc['event']){ $contract->event = null; }
-                if(!$dc['capita']){ $contract->capita = null; }
-                if(!$dc['pgp']){ $contract->pgp = null; }*/
                 if($contract){
                     $contracts[] = $contract;
                 }
