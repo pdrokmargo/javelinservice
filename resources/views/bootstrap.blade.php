0be940cb7d6116b5ab2348397cfcba39c573f25c
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-04-03 00:06:30 -0500

    asd

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -31,7 +31,7 @@
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" />
 
   <base href="/">
-  <link href="/css/main.039c07efb6500b5a8bec.css" rel="stylesheet">
+  <link href="/css/main.e4a5d006cf5aea7c2baf.css" rel="stylesheet">
 </head>
 
 <body id="body">
@@ -43,9 +43,9 @@
 
 
 
-  <script type="text/javascript" src="/js/polyfills.039c07efb6500b5a8bec.js"></script>
-  <script type="text/javascript" src="/js/vendor.039c07efb6500b5a8bec.js"></script>
-  <script type="text/javascript" src="/js/main.039c07efb6500b5a8bec.js"></script>
+  <script type="text/javascript" src="/js/polyfills.e4a5d006cf5aea7c2baf.js"></script>
+  <script type="text/javascript" src="/js/vendor.e4a5d006cf5aea7c2baf.js"></script>
+  <script type="text/javascript" src="/js/main.e4a5d006cf5aea7c2baf.js"></script>
 </body>
 
 </html>
