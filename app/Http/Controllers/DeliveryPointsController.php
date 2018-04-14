a855a98b98e2ea8190069a293748b9ee7199e8cd
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-04-13 23:16:47 -0500

    asd

--- app/Http/Controllers/DeliveryPointsController.php
+++ app/Http/Controllers/DeliveryPointsController.php
@@ -128,7 +128,7 @@ class DeliveryPointsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, $id)
     {
         DB::beginTransaction();
         try
