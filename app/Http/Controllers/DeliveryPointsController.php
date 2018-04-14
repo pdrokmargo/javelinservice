0115cc881fab497bd589a61ee629087e7fa167e4
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-04-13 23:14:45 -0500

    asd

--- app/Http/Controllers/DeliveryPointsController.php
+++ app/Http/Controllers/DeliveryPointsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 class DeliveryPointsController extends Controller
 {
