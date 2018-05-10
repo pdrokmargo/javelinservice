7d59aade1067213d80a2c1612608adee33f84ba4
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-05-10 00:00:03 -0500

    asd

--- app/Http/Controllers/AffiliatesController.php
+++ app/Http/Controllers/AffiliatesController.php
@@ -23,7 +23,7 @@ class AffiliatesController extends Controller
             
             $query = DB::table('affiliates as a')
             ->join('delivery_contracts as dc', 'dc.id', '=', 'a.delivery_contract_id')
-            ->join('collections_values as cv', 'cv.id', '=', 'a.contracts_payment_method_id')
+            //->leftJoin('collections_values as cv', 'cv.id', '=', 'a.contracts_payment_method_id')
             ->select(DB::raw("a.id, (a.firstname || ' ' || a.middlename || ' ' || a.lastname1 || ' ' || a.lastname2) as name, dc.name as contract, cv.value as contracts_payment_method, a.state"));
 
             if ($search != '') {
@@ -32,7 +32,6 @@ class AffiliatesController extends Controller
                 $query = $query->where('a.delete', false)->orderBy($ordername, $ordertype);
             }    
             
-            return $query->toSql();
 
             $data=[];  
             if ($page) {
