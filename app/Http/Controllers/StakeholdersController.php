cba29b4a2cedabf063108e1c75e59bfc5d76a08e
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-05-18 22:38:58 -0500

    asd

--- app/Http/Controllers/StakeholdersController.php
+++ app/Http/Controllers/StakeholdersController.php
@@ -324,7 +324,7 @@ class StakeholdersController extends Controller
             $data = json_decode($request->data,true);
             $stakeholders_info = $data["stakeholders_info"];
             $comercial_stakeholders_info = $data["comercial_stakeholders_info"];
-            $comercial_stakeholders_info["tax_regime_id"] = 0;
+            
             $customer = $data["customer"];
             $employee = $data["employee"];
             $supplier = $data["supplier"];
