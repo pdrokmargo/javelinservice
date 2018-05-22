58805bc51ac8ed548660322fb3f2a8fa62a2dc4b
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-05-21 22:13:50 -0500

    asd

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -1,46 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-    <meta charset="utf-8" />
-    <title>Javelin</title>
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-touch-fullscreen" content="yes">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- google fonts -->
-    <script type="text/javascript">
-      WebFontConfig = {
-        google: { families: [ 'Roboto:100,300,400,400italic,500,700:latin' ] }
-      };
-      (function() {
-        var wf = document.createElement('script');
-        wf.src = 'vendors/webfontloader/webfontloader.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-      })(); 
-    </script>
+<head>
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+  <meta charset="utf-8" />
+  <title>Javelin</title>
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-touch-fullscreen" content="yes">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="vendors/loader.css" rel="stylesheet" >
-    <link rel="stylesheet" href="vendors/material-design-icons/iconfont/material-icons.css" rel="stylesheet" >
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css"/>
+  <!-- google fonts -->
+  <script type="text/javascript">
+    WebFontConfig = {
+      google: { families: ['Roboto:100,300,400,400italic,500,700:latin'] }
+    };
+    (function () {
+      var wf = document.createElement('script');
+      wf.src = 'vendors/webfontloader/webfontloader.js';
+      wf.type = 'text/javascript';
+      wf.async = 'true';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wf, s);
+    })(); 
+  </script>
 
-    <base href="/">
-  <link href="/css/main.1298306958df12cfdc70.css" rel="stylesheet"></head>
+  <link rel="stylesheet" href="vendors/loader.css" rel="stylesheet">
+  <link rel="stylesheet" href="vendors/material-design-icons/iconfont/material-icons.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" />
 
-  <body id="body">
+  <base href="/">
+  <link href="/css/main.e5606e30734ef6964aa9.css" rel="stylesheet">
+</head>
 
-    <app-root>
-      <div id="loader-container"></div>
-    </app-root>
+<body id="body">
+
+  <app-root>
+    <div id="loader-container"></div>
+  </app-root>
 
 
 
-    
-<script type="text/javascript" src="/js/polyfills.1298306958df12cfdc70.js"></script><script type="text/javascript" src="/js/vendor.1298306958df12cfdc70.js"></script><script type="text/javascript" src="/js/main.1298306958df12cfdc70.js"></script></body>
+
+  <script type="text/javascript" src="/js/polyfills.e5606e30734ef6964aa9.js"></script>
+  <script type="text/javascript" src="/js/vendor.e5606e30734ef6964aa9.js"></script>
+  <script type="text/javascript" src="/js/main.e5606e30734ef6964aa9.js"></script>
+</body>
 
 </html>
