4dfefda3913750dac40a92bb8981793a4b9051ab
SANCHEZSJD <jd_sanchez1993@hotmail.es>
2018-06-04 22:41:38 -0500

    fghjk

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -30,7 +30,7 @@
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css"/>
 
     <base href="/">
-  <link href="/css/main.27f16e26170f515cd797.css" rel="stylesheet"></head>
+  <link href="/css/main.163f41ef38a7b9c8ce93.css" rel="stylesheet"></head>
 
   <body id="body">
 
@@ -41,6 +41,6 @@
 
 
     
-<script type="text/javascript" src="/js/polyfills.27f16e26170f515cd797.js"></script><script type="text/javascript" src="/js/vendor.27f16e26170f515cd797.js"></script><script type="text/javascript" src="/js/main.27f16e26170f515cd797.js"></script></body>
+<script type="text/javascript" src="/js/polyfills.163f41ef38a7b9c8ce93.js"></script><script type="text/javascript" src="/js/vendor.163f41ef38a7b9c8ce93.js"></script><script type="text/javascript" src="/js/main.163f41ef38a7b9c8ce93.js"></script></body>
 
 </html>
