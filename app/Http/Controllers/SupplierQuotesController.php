cecf252b77e24475dc1e52f0d023e1dce8af813e
jama303 <johnmartinez.jm.jm@gmail.com>
2018-09-12 17:05:58 -0500

    Controller Suppliers Quotes

--- app/Http/Controllers/SupplierQuotesController.php
+++ app/Http/Controllers/SupplierQuotesController.php
@@ -6,8 +6,37 @@ use Illuminate\Http\Request;
 
 class SupplierQuotesController extends Controller
 {
-    public function index(Request $request)    {       
-        $data =  \App\Models\SupplierQuotes::all();
-        return response()->json(['status'=>'success', "message"=>'', "data" => $data ], 200);
+    public function index(Request $request)
+    {
+        try {
+          
+            $search = isset($request->search) ? '%'.strtolower($request->search).'%' : '';
+            $ordername = isset($request->ordername) ? $request->ordername : 'w.id';
+            $ordertype = isset($request->ordertype) ? $request->ordertype : 'DESC';
+            $page = $request->page;
+            $company_id = $request->user()->company_default_id;
+
+            $query = DB::table('suppliers_quotes as s')
+            ->select(DB::raw('s.id, s.supplier_id, s.created_at, s.status'));
+
+            /*if ($search!='') {
+                $query = $query->where('s.delete', false)->whereRaw("s.id = ? and (s.supplier_id like ? or c.value like ? or (case when w.state=true then 'activa' else 'inactiva' end) like ?)", array($company_id, $search, $search, $search))
+                ->orderBy($ordername, $ordertype);
+            }else{
+                $query=$query->where('w.delete', false)->where('w.company_id', $company_id)->orderBy($ordername, $ordertype);
+            } */
+
+            $data=[];  
+            if ($page) {
+              $data=$query->paginate(30);
+            }else{
+              $data=$query->get();
+            }  
+            
+            return response()->json(['status'=>'success', "message"=>'', "data" => $data ], 200);
+
+      } catch (Exception $e) {
+          return 'Error:'.$e->getMessage();
+      } 
     }
 }
