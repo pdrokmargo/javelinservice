d85e104d9a3078c0f92199d38b69fe326ac88b2f
jama303 <johnmartinez.jm.jm@gmail.com>
2018-09-14 12:22:50 -0500

    ss

--- app/Http/Controllers/SupplierQuotesController.php
+++ app/Http/Controllers/SupplierQuotesController.php
@@ -22,7 +22,7 @@ class SupplierQuotesController extends Controller
 
             $query = new \App\Models\SupplierQuotes();
             if ($search!='') {
-                $query = $query->whereRaw("status = false and code like ? or lower(supplier_quotes.id) like ? or lower(notes) like ? lower(created_at) like ? or (case when status=true then 'activo' else 'inactivo' end) like ?", array($search, $search, $search, $search, $search))
+                $query = $query->whereRaw("status = true and code like ? or lower(supplier_quotes.id) like ? or lower(notes) like ? lower(created_at) like ? or (case when status=true then 'activo' else 'inactivo' end) like ?", array($search, $search, $search, $search, $search))
                 ->orderBy($ordername, $ordertype);
             }else{
                 $query=$query->where('status', false)->orderBy($ordername, $ordertype);
