cf607c526f45ce33c3e7c0134d7e8d30730f3c5a
jama303 <johnmartinez.jm.jm@gmail.com>
2018-09-17 11:41:50 -0500

    sq

--- app/Http/Controllers/SupplierQuotesController.php
+++ app/Http/Controllers/SupplierQuotesController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Response;
+
 
 class SupplierQuotesController extends Controller
 {
