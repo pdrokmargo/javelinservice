effb32a06b6852d28c7d6fc59329c160ea160214
jama303 <johnmartinez.jm.jm@gmail.com>
2018-09-19 10:25:32 -0500

    Suppliers Quotes

--- app/Http/Controllers/SupplierQuotesController.php
+++ app/Http/Controllers/SupplierQuotesController.php
@@ -53,7 +53,8 @@ class SupplierQuotesController extends Controller
         try {
             
             $data = json_decode($request->data, true);
-            $supplier_quotes=\App\Models\SupplierQuotes::create($data);
+            $supplier_quotes->create($data);
+            $this->CreateLog($request->user()->id, 'suppliers_quotes', 1,'');
             DB::commit();
             return response()->json([ 
                 "store" => true, 
