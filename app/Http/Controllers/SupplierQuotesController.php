4d725cfbd0d877a0b3f36e41e6d4cbff133f476e
jama303 <johnmartinez.jm.jm@gmail.com>
2018-09-28 22:42:06 -0500

    ss

--- app/Http/Controllers/SupplierQuotesController.php
+++ app/Http/Controllers/SupplierQuotesController.php
@@ -23,10 +23,10 @@ class SupplierQuotesController extends Controller
 
             $query = new SupplierQuotes();
             if ($search!='') {
-                $query = $query->whereRaw("status = true and stakeholderInfo.firstname like ?", array($search) )
-                    /*->with(["stakeholderInfo"=>function($query)use($search){
+                $query = $query->whereRaw("status = true " )
+                    ->with(["stakeholderInfo"=>function($query)use($search){
                     $query->where("firstname", $search);
-                }])*/
+                }])
                 ->orderBy($ordername, $ordertype);
             }else{
                 $query=$query->where('status', false)->orderBy($ordername, $ordertype);
