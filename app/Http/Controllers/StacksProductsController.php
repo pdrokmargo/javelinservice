632fac2e7acd5f56857e4f9980b96030f7f5e0fd
Jesu Sanchez <jd_sanchez1993@hotmail.es>
2018-09-30 01:04:29 -0500

    as

--- app/Http/Controllers/StacksProductsController.php
+++ app/Http/Controllers/StacksProductsController.php
@@ -16,7 +16,10 @@ class StacksProductsController extends Controller
     public function index(Request $request)
     {
         $data = json_decode($request->data, true);
-        $data = new \App\Models\StocksProducts::whereBetween('expiration_date',[$data['from'], $data['to']])
+        $to = date_format(date($data['to']),'Y-m-d');
+        $from = date_format(date($data['from']),'Y-m-d');
+        
+        $data = new \App\Models\StocksProducts::whereBetween('expiration_date',[$to, $from])
         ->where('warehouse_id', $data['warehouse_id'])
         ->with(['products' => function($query){
             $query->select('sku','name','units','delivery_fraction')
