d64c729453a73f81c1c401f67c96b186d31d5f70
Jesu Sanchez <jd_sanchez1993@hotmail.es>
2018-10-02 22:23:09 -0500

    update

--- app/Http/Controllers/StacksProductsController.php
+++ app/Http/Controllers/StacksProductsController.php
@@ -17,19 +17,18 @@ class StacksProductsController extends Controller
     {
         $data = json_decode($request->data, true);
 
-        /*$to = new DateTime($data['to']);
-        $from = new DateTime($data['from']);*/
+        $to = new DateTime($data['to']);
+        $from = new DateTime($data['from']);
 
-        $data = new \App\Models\StocksProducts::all();
-        /*where('expiration_date','>=',$from)
+        $rs = new \App\Models\StocksProducts::where('expiration_date','>=',$from)
         ->where('expiration_date','<=',$to)
         ->where('warehouse_id', $data['warehouse_id'])
         ->with(['products' => function($query){
             $query->select('sku','name','units','delivery_fraction')
-        }])->get();*/
+        }])->get();
 
         return response()->json([
-            "data" => $data 
+            "data" => $rs 
         ], 200);
     }
 
