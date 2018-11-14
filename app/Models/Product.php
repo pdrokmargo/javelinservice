f8b493cc69ff639f6caae66b1dd373e067919abd
Pedro Camargo <pdro_kmargo@hotmail.com>
2018-11-14 01:20:25 -0500

    w

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -47,7 +47,7 @@ class Product extends Model
         'pharmaceutical_drug' => 'array',
 	];
 
-	protected $with = array('pharmaceutical_drug_obj');
+	protected $with = array('PharmaceuticalDrug');
 
 	public function getPharmaceuticalDrugAttribute(){
 		$item = json_decode($this->attributes['pharmaceutical_drug']);
