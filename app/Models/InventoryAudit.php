8bd157daae5fb34522c8400abd80328a6508b2cd
Jesus Sanchez <jd_sanchez1993@hotmail.es>
2019-02-02 22:52:53 -0500

    Update InventoryAudit.php

--- app/Models/InventoryAudit.php
+++ app/Models/InventoryAudit.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class InventoryAudit extends Model
 {
-    //
+    use \App\Uuids;
+    protected $table = 'inventory_audit';
+    public $incrementing = false;
+    protected $fillable = [
+        'inventory_adjustment_type_id',
+        'warehouse_id',
+        'user_id',
+        'description',
+        'blinded_qty',
+        'date',
+        'audit_state_id'
+    ];
 }
