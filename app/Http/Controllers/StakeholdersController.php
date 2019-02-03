7d2e620aee7ed1e8650d74a5d588f0b92cf9370e
Pedro Camargo <pdro_kmargo@hotmail.com>
2019-02-03 12:18:43 -0500

    improvement

--- app/Http/Controllers/StakeholdersController.php
+++ app/Http/Controllers/StakeholdersController.php
@@ -129,7 +129,7 @@ class StakeholdersController extends Controller
                     break;
                 case '2':
                     $table='suppliers';
-                    $stakeholders_params =  'x.sales_contact, x.payment_condition_id, x.global_discount';
+                    $stakeholders_params =  'x.sales_contact, x.payment_condition_id, x.global_discount, ';
                     break;
                 case '3':
                     $table='employees';
