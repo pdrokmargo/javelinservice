40a3aa02540e0af60f1ed98e622a41485b676087
Jesus Sanchez <jd_sanchez1993@hotmail.es>
2019-02-03 14:46:34 -0500

    guardar inventario de auditoria

--- app/Http/Controllers/InventoryAuditController.php
+++ app/Http/Controllers/InventoryAuditController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\Models\InventoryAudit;
+use \App\Models\InventoryAuditDetail;
+use DB;
 
 class InventoryAuditController extends Controller
 {
@@ -50,7 +52,45 @@ class InventoryAuditController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        DB::beginTransaction(); 
+        try {
+            $data = json_decode($request->data);
+            $InventoryAudit = [
+                'inventory_adjustment_type_id' => '',
+                'warehouse_id'      => $data->warehouse_id,
+                'user_id'           => $data->user_id,
+                'description'       => '',
+                'blinded_qty'       => $data->blinded_qty,
+                'date'              => $date->date,
+                'audit_state_id'    => 189
+            ];
+
+            $inventory_audit_id = InventoryAudit::create($InventoryAudit)->id;
+            $InventoryAuditDetail = [];
+            foreach ($data->details as $key => $value) {
+                $InventoryAuditDetail[] = [
+                    'inventory_audit_id'        => $inventory_audit_id,
+                    'stock_product_id'          => $value["id"],
+                    'physical_set_stock'        => $value["set_stock"],
+                    'physical_fraction_stock'   => $value["fraction_stock"]
+                ];
+            }
+
+            InventoryAuditDetail::create($InventoryAuditDetail);
+
+            DB::commit();
+            return response()->json([ 
+                "store" => true, 
+                "message" => "Registro almacenado" 
+            ], 200);
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json([ 
+                "store" => false, 
+                "message" => "Error al intentar almacenar el nuevo registro" 
+            ], 400);
+        }
+
     }
 
     /**
--- app/Models/InventoryAuditDetail.php
+++ app/Models/InventoryAuditDetail.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class InventoryAuditDetail extends Model
 {
-    //
+    use \App\Uuids;
+    protected $table = 'inventory_audit_details';
+    public $incrementing = false;
+    protected $fillable = [
+        'inventory_audit_id',
+        'stock_product_id', 
+        'physical_set_stock',
+        'physical_fraction_stock',
+    ];
 }
