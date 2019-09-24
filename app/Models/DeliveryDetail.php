5ad0985263ff542cc3a021e47ba31c5532aea698
Pedro Camargo <pdro_kmargo@hotmail.com>
2019-09-24 11:10:20 -0500

    qq

--- app/Models/DeliveryDetail.php
+++ app/Models/DeliveryDetail.php
@@ -14,6 +14,7 @@ class DeliveryDetail extends Model
             'product_id',
             'requested_units',
             'delivered_units',
+            'delivery_number',
             'batch',
             'expiration_date',
             'synced'
