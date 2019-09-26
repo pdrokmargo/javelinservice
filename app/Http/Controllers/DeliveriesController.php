fff46e349d681deb0794e61d6c7c757154ba5cf1
Pedro Camargo <pdro_kmargo@hotmail.com>
2019-09-26 00:10:35 -0500

    qq

--- app/Http/Controllers/DeliveriesController.php
+++ app/Http/Controllers/DeliveriesController.php
@@ -111,7 +111,7 @@ class DeliveriesController extends Controller
     public function show($id)
     {
         $delivery = \App\Models\Delivery::find($id);
-        $delivery = $delivery->load('details');
+        $delivery = $delivery->load('details.product');
 	    return response()->json(["status"=>"success", "message"=>"", "data" =>$delivery ], 200);
     }
 
