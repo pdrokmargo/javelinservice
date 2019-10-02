86d7e07545a4d863f2b90e0c320394fb8ed50502
Pedro Camargo <pdro_kmargo@hotmail.com>
2019-10-01 23:35:00 -0500

    qqq

--- app/Http/Controllers/DeliveriesController.php
+++ app/Http/Controllers/DeliveriesController.php
@@ -30,13 +30,13 @@ class DeliveriesController extends Controller
 
     public function scheduled_deliveries($affiliate_id)
     {
-        $deliveries = \App\Models\ScheduledDelivery::where('affiliate_id', $affiliate_id)->where('status',true)->whereNull('delivery_fulfillment_id')->orderBy('consecutive', 'ASC')->paginate(15); 
+        $deliveries = \App\Models\ScheduledDelivery::where('affiliate_id', $affiliate_id)->where('status',true)->whereNull('delivery_fulfillment_id')->paginate(15); 
         return response()->json(['status'=>'success', "message"=>'', "scheduled-deliveries" => $deliveries ], 200);
     }
 
     public function affiliate_deliveries($affiliate_id)
     {
-        $deliveries = \App\Models\DeliveryDetail::with(['delivery.delivery_point' => function($query){$query->where('affiliate_id', $affiliate_id);}])->orderBy('consecutive', 'ASC')->paginate(15); 
+        $deliveries = \App\Models\DeliveryDetail::with(['delivery.delivery_point' => function($query){$query->where('affiliate_id', $affiliate_id);}])->paginate(15); 
         return response()->json(['status'=>'success', "message"=>'', "affiliate-deliveries" => $deliveries ], 200);
     }
 
