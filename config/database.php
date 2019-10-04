89a692e97c13cb1f1024b5fd1146a722262076fd
Pedro Camargo <pdro_kmargo@hotmail.com>
2019-10-04 09:43:04 -0500

    qqq

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'main'),
+    'default' => env('DB_CONNECTION', 'local'),
 
     /*
     |--------------------------------------------------------------------------
