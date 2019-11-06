238a53705de554aec958f5711abce0684471b8f1
Pedro Camargo <pdro_kmargo@hotmail.com>
2019-11-06 00:14:20 -0500

    qq

--- app/Models/SupplierOrder.php
+++ app/Models/SupplierOrder.php
@@ -54,7 +54,7 @@ class SupplierOrder extends Model
             }
             
         }
-        return $details_received;
+        return $details_out;
     }
     public function stakeholderInfo() {
         return $this->hasOne('App\Models\StakeholdersInfo', 'id','supplier_id');
