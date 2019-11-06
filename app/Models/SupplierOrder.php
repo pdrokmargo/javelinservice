1e348f864aad87f4e368c865c358023516fd426f
Pedro Camargo <pdro_kmargo@hotmail.com>
2019-11-05 21:14:49 -0500

    qq

--- app/Models/SupplierOrder.php
+++ app/Models/SupplierOrder.php
@@ -40,8 +40,8 @@ class SupplierOrder extends Model
         
         $details = json_decode($this->products,true);
         foreach($details as $d){
-            $product_units = $details_received->where('product_id', $d->product_id)->where('fraction', false);
-            $product_fractions = $details_received->where('product_id', $d->product_id)->where('fraction', true);
+            $product_units = $details_received->where('product_id', $d['product_id'])->where('fraction', false);
+            $product_fractions = $details_received->where('product_id', $d['product_id'])->where('fraction', true);
             $d['fraction'] -= $product_fractions->sum('units');
             $d['units'] -= $product_units->sum('units');
             $d['purchase_price'] = $d['product']['averageunitcost'];
