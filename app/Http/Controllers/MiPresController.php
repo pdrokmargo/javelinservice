88be6144ab73d75b9b8969fdb8a94f2de5cc599f
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-10-24 11:04:20 -0500

    fds

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -25,6 +25,22 @@ class MiPresController extends Controller
     }
     public function direccionamientoXPrescripcion(Request $request, $prescription)
     {
+        $client = new \GuzzleHttp\Client();
+$response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+
+echo $response->getStatusCode(); // 200
+echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
+// Send an asynchronous request.
+$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo 'I completed! ' . $response->getBody();
+});
+
+$promise->wait();
+        //     $url = $this->baseUrl.'DireccionamientoXPrescripcion/'.$this->nit.'/_0hZFuEPhyPIbwAowjiePR8TMae8cIdhF4MCV5Dh7CA=/'.$prescription;
+        // $httpClient = new HttpClient(['base_uri' => $url]);
         // $this->generateToken();
         
         
@@ -41,32 +57,32 @@ class MiPresController extends Controller
 
         // $promise->wait();
 
-        try {
+        // try {
 
-            $client = new Client();
-            $url = $this->baseUrl.'DireccionamientoXPrescripcion/'.$this->nit.'/_0hZFuEPhyPIbwAowjiePR8TMae8cIdhF4MCV5Dh7CA=/'.$prescription;
-            $res = $client->request('GET', $url);
+        //     $client = new Client();
+        //     $url = $this->baseUrl.'DireccionamientoXPrescripcion/'.$this->nit.'/_0hZFuEPhyPIbwAowjiePR8TMae8cIdhF4MCV5Dh7CA=/'.$prescription;
+        //     $res = $client->request('GET', $url);
                 
-            // Here the code for successful request
+        //     // Here the code for successful request
         
-        } catch (RequestException $e) {
+        // } catch (RequestException $e) {
         
-            // Catch all 4XX errors 
+        //     // Catch all 4XX errors 
             
-            // To catch exactly error 400 use 
-            if ($e->hasResponse()){
-                if ($e->getResponse()->getStatusCode() == '400') {
-                        echo $e;
-                }
-            }
+        //     // To catch exactly error 400 use 
+        //     if ($e->hasResponse()){
+        //         if ($e->getResponse()->getStatusCode() == '400') {
+        //                 echo $e;
+        //         }
+        //     }
         
-            // You can check for whatever error status code you need 
+        //     // You can check for whatever error status code you need 
             
-        } catch (\Exception $e) {
-            dd($e);
-            // There was another exception.
+        // } catch (\Exception $e) {
+        //     dd($e);
+        //     // There was another exception.
         
-        }
+        // }
         
     }
 }
