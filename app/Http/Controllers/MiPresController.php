edcba58e926a4afb943a6c242a0f211ee5a01bab
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-10-23 20:22:38 -0500

    qq

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -11,25 +11,30 @@ class MiPresController extends Controller
     private $mainToken = '525FE1ED-00E2-4364-9F5D-7612B8B1E21E';
     private $nit = '802024817';
     private $secondToken = '';
+    private $client;
+
+    function __construct() {
+        $client = new Client();
+    }
 
     public function generateToken()
     {
-        $client = new Client();
+        // $client = new Client();
         $this->secondToken = $client->request('GET', $this->baseUrl.'GenerarToken/'.$this->nit.'/'.$this->mainToken);
         return $this->secondToken;
     }
     public function direccionamientoXFecha(Request $request)
     {
-        $client = new Client();
+        // $client = new Client();
         $res = $client->request('GET', 'GenerarToken/'.$this->nit.'/'.$mainToken);
         return $res;
     }
     public function direccionamientoXPrescripcion(Request $request, $prescription)
     {
-        $this->generateToken();
-        $client = new Client();
+        // $this->generateToken();
+        // $client = new Client();
         echo $prescription;
-        $res = $client->request('GET', $this->baseUrl.'DireccionamientoXPrescripcion/'.$this->nit.'/'.$this->secondToken.'/'.$prescription);
+        $res = json_decode ($client->request('GET', $this->baseUrl.'DireccionamientoXPrescripcion/'.$this->nit.'/'.$this->secondToken.'/'.$prescription)->getBody()->getContents());
         // $res = (string)$res->getBody();
         dd($res);
         // return response()->json([ 
