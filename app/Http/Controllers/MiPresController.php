39781450003b1104328b8262e1cb33b2ae5a1ea3
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-10-24 10:18:12 -0500

    dsad

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -37,10 +37,10 @@ class MiPresController extends Controller
 
         $request = new \GuzzleHttp\Psr7\Request('GET', $url);
         $promise = $client->sendAsync($request)->then(function ($response) {
-            // echo 'I completed! ' . $response->getBody();
-            return response()->json([ 
-                "object" => $response->getBody()
-            ], 200);
+            echo 'I completed! ' . $response->getBody();
+            // return response()->json([ 
+            //     "object" => $response->getBody()
+            // ], 200);
         });
 
         $promise->wait();
