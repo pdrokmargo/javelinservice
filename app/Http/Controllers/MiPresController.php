2144201756c72c9c6bab2f5b93fd42459399f58e
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-10-24 10:20:13 -0500

    dasd

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -10,7 +10,7 @@ class MiPresController extends Controller
     private $baseUrl = 'https://wsmipres.sispro.gov.co/WSSUMMIPRESNOPBS/api/';
     private $mainToken = '525FE1ED-00E2-4364-9F5D-7612B8B1E21E';
     private $nit = '802024817';
-    private $secondToken = '_0hZFuEPhyPIbwAowjiePR8TMae8cIdhF4MCV5Dh7CA=';
+    private $secondToken = '_0hZFuEPhyPIbwAowjiePR8TMae8cIdhF4MCV5Dh7CA%3D';
     // private $client;
 
     // public function __construct() {
