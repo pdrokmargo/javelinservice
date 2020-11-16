9626174aa8d0bbb6d8db231171df14054ff7e8e3
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-11-16 02:45:17 -0500

    dsa

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -160,7 +160,7 @@ class MiPresController extends Controller
                 $client = new \GuzzleHttp\Client();
                 $headers = ['Accept' => 'application/json'];
                 $form_params = [];
-                dump($request);
+                dump($request["ID"]);
                 $endpoint = '';
                 if($process == 'programming'){
                     $endpoint = 'Programacion';
