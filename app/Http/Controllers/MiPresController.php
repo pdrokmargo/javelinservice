fa0c17eb0315121de8249b98d31e0eea01530224
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-11-26 07:48:08 -0500

    fds

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -24,51 +24,6 @@ class MiPresController extends Controller
         $secondToken = $client->request('GET', $this->baseUrl.'GenerarToken/'.$this->nit.'/'.$this->mainToken);
         return $secondToken;
     }
-    // public function prescription(Request $request, $token, $prescription)
-    // {   
-    //     $keys = ['addressing', 'programming', 'delivery', 'delivery-report'];
-    //     $finalData = ['addressing' => '', 'programming' => '', 'delivery' => '', 'delivery-report' => ''];
-    //     // $endpoints = ['DireccionamientoXPrescripcion', 'ProgramacionXPrescripcion', 'EntregaXPrescripcion', 'ReporteEntregaXPrescripcion'];
-    //     $final = [];
-    //     $prescriptions = [];
-        
-    //     try {
-    //         foreach ($keys as $k){
-    //             $client = new \GuzzleHttp\Client();
-    //             $headers = ['Accept' => 'application/json'];
-    //             $endpoint = '';
-    //             if($k == 'addressing'){
-    //                 $endpoint = 'DireccionamientoXPrescripcion';
-    //             }else if($k == 'programming'){
-    //                 $endpoint = 'ProgramacionXPrescripcion';
-    //             }else if($k == 'delivery'){
-    //                 $endpoint = 'EntregaXPrescripcion';
-    //             }else if($k == 'delivery-report'){
-    //                 $endpoint = 'ReporteEntregaXPrescripcion';
-    //             }                
-    //             $url = $this->baseUrl.$endpoint.'/'.$this->nit.'/'.$token.'/'.$prescription;
-    //             $response = $client->request('GET', $url, $headers);
-    //             $body = $response->getBody();
-    //             $status = 'true';
-    //             $message = 'Data found!';
-    //             $data = json_decode($body);
-    //             $finalData[$k] = $data;
-    //         }
-    //     }catch(ClientException $ce){
-    //         $status = 'false';
-    //         $message = $ce->getMessage();
-    //         $data = [];
-    //     }catch(RequestException $re){
-    //        $status = 'false';
-    //        $message = $re->getMessage();
-    //        $data = [];
-    //     }catch(Exception $e){
-    //        $this->status = 'false';
-    //        $this->message = $e->getMessage();
-    //        $data = [];
-    //     }
-    //     return ['status'=>$status,'message'=>$message,'data'=>$finalData];        
-    // }
     public function getPrescriptions(Request $request, $token)
     {   
         //There are 3 cases, we just cover to fetch one prescription without details given a prescription number.
@@ -96,8 +51,8 @@ class MiPresController extends Controller
            $message = $re->getMessage();
            $data = [];
         }catch(Exception $e){
-           $this->status = 'false';
-           $this->message = $e->getMessage();
+           $status = 'false';
+           $message = $e->getMessage();
            $data = [];
         }
         }else{
