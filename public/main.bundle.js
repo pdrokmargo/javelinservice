1e3f0fba68ec8668e5b7bc1d43489f6f5ecb3889
Pedro Camargo <pdro_kmargo@hotmail.com>
2020-12-11 15:47:28 -0500

    1.0.4

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -845,7 +845,7 @@ function makeAppConfig() {
         brand: 'Javelin',
         user: 'Lisa',
         year: year,
-        version: 'v1.0.3',
+        version: 'v1.0.4',
         layoutBoxed: false,
         navCollapsed: false,
         navBehind: false,
