8e6e0951235527e8f052fb4580707449d00fcbc7
Pedro Camargo <pdro_kmargo@hotmail.com>
2021-02-21 10:33:58 -0500

    model update cums products mipres

--- app/Models/CumsProductosMipres.php
+++ app/Models/CumsProductosMipres.php
@@ -8,4 +8,8 @@ class CumsProductosMipres extends Model
 {
     public $timestamps = false;
     protected $table = 'cums_productos_mipres';
+    public $incrementing = false;  
+    protected $fillable = [
+        'cums'
+    ];
 }
