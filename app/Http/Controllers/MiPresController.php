57eb71487e7be78f93ca702ab9afe533ce0ed33a
Pedro Camargo <pdro_kmargo@hotmail.com>
2021-04-16 08:59:48 -0500

    test

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -28,7 +28,7 @@ class MiPresController extends Controller
         // }
         try {
             $sectok = \App\Models\Configuration::where('code', 'mipresSecondToken')->first();
-            $secondToken = $sectok == null ? '' : json_decode($sectok->value, true);
+            $secondToken = $sectok == null ? '' : json_decode($sectok->value, true)["value"];
         }catch(Exception $e){
             $secondToken = '-1';
         }
