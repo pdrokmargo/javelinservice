f05a62a32dce256ac4fa36a9c383f00768cf2d2e
Pedro Camargo <pdro_kmargo@hotmail.com>
2021-05-02 23:00:20 -0500

    dsa

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -58,7 +58,7 @@ class MiPresController extends Controller
                 $secondToken = $client->request('GET', $this->baseUrl.'GenerarToken/'.$this->nit.'/'.$this->mainToken, ['timeout' => 30]);
                 $t['token'] = $secondToken;
                 $t['expiration'] = Carbon::now()->addHours(8);
-                $sectok->value = json_encode($t, true);
+                $sectok->value = "1";
                 $sectok->save();
                 echo '2';
             }else{
