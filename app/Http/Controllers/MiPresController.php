72c4ae55e1807d6c6a722707e19fd1d6f1ffb224
Pedro Camargo <pdro_kmargo@hotmail.com>
2021-05-05 16:40:11 -0500

    sd

--- app/Http/Controllers/MiPresController.php
+++ app/Http/Controllers/MiPresController.php
@@ -19,7 +19,7 @@ class MiPresController extends Controller
     private $baseUrl = 'https://wsmipres.sispro.gov.co/WSSUMMIPRESNOPBS/api/';
     private $mainToken = '525FE1ED-00E2-4364-9F5D-7612B8B1E21E';
     private $nit = '802024817';
-    protected $secondToken = '';
+    protected $secondToken = ''; 
     protected $client;
 
     public function generateToken(Request $request)
