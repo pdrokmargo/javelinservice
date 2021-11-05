127826146e6ead1d2ede56e59b70e2f4b5029318
Pedro Camargo <pdro_kmargo@hotmail.com>
2021-11-05 02:27:36 -0500

    qq

--- app/Models/SupplierOrder.php
+++ app/Models/SupplierOrder.php
@@ -37,13 +37,12 @@ class SupplierOrder extends Model
     public function getRemainingAttribute()
     {
         $details_received = \App\Models\InventoryMovementDetail::with(['inventory_movement' => function ($query) {
-            $query->where('document_fullfilled_id', $this->id);
+            $query->where('inventory_movement.document_fullfilled_id', $this->id);
         }]);
         $details = json_decode($this->products,true);
         $details_out = [];
         foreach($details as $d){
-            // $d['units'] -= $details_received->where('product_id', $d['product_id'])->where('fraction', false)->sum('units');
-            $d['units'] = $details_received->where('fraction', false)->sum('units');
+            $d['units'] -= $details_received->where('product_id', $d['product_id'])->where('fraction', false)->sum('units');
             $d['purchase_price'] = $d['product']['averageunitcost'];
             if($d['units'] > 0){
                 $details_out[] = $d;
